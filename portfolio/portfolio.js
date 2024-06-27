dde11007b457466519bfa0484b9d24debcb10dba
DURGANJALI GADAMSETTY <147343685+G-DURGANJALI@users.noreply.github.com>
2024-06-27 22:59:27 +0530

    Update portfolio.js

--- portfolio/portfolio.js
+++ portfolio/portfolio.js
@@ -1,1 +1,18 @@
-
+// scroll sections
+let sections=document.querySelectorAll('section');
+let navlinks=document.querySelectorAll('header nav a');
+Window.onscroll=()=>{
+	sections.forEach(sec=>{
+		let top=window.scrollY;
+		let offset=sec.offsetTop -100;
+		let height=sec.offsetHeight;
+		let id=sec.getAttribute('id');
+		      if(top>=offset&&top<offset+height)
+			   {
+				   navlinks.forEach(links=>{
+					links.classList.remove('active');
+					document.querySelectorAll('header nav a[href*=' +id +']')).classList.add('active');
+				});
+			
+	 });
+ }
