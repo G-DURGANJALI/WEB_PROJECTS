89739e50a7dc9046bf81bd0451eef30f2a4fd892
DURGANJALI GADAMSETTY <147343685+G-DURGANJALI@users.noreply.github.com>
2024-07-10 19:14:26 +0530

    Update portfolio.js

--- portfolio/portfolio.js
+++ portfolio/portfolio.js
@@ -1,18 +1,19 @@
-// scroll sections
-let sections=document.querySelectorAll('section');
-let navlinks=document.querySelectorAll('header nav a');
-Window.onscroll=()=>{
-	sections.forEach(sec=>{
-		let top=window.scrollY;
-		let offset=sec.offsetTop -100;
-		let height=sec.offsetHeight;
-		let id=sec.getAttribute('id');
-		      if(top>=offset&&top<offset+height)
-			   {
-				   navlinks.forEach(links=>{
-					links.classList.remove('active');
-					document.querySelectorAll('header nav a[href*=' +id +']')).classList.add('active');
-				});
-			
-	 });
- }
+ScrollReveal({
+	reset : true,
+	distance:'80px',
+	duration : 200,
+	delay:300
+
+});
+
+ScrollReveal().reveal('.home-content ,.heading',{origin:'top' ,duration:5000});
+ScrollReveal().reveal('.projects-box ',{origin : 'bottom' ,duration:3000});
+ScrollReveal().reveal('.skills-column,.education-column',{origin : 'left' ,duration:3000});
+// typed js
+const typed= new Typed('.name',{
+	strings: [' Frontend Developer '],
+	typeSpeed :300,
+	backSpeed:300,
+	backDelay :300,
+	loop:true
+});
