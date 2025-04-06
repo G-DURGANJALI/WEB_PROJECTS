ed767d3a70c9a331154d28189879df08ef54ba13
DURGANJALI GADAMSETTY <147343685+G-DURGANJALI@users.noreply.github.com>
2025-04-06 22:17:06 +0530

    Update MyApplications.jsx

--- MERN-PROJECTS/job-portal/frontend/src/components/Application/MyApplications.jsx
+++ MERN-PROJECTS/job-portal/frontend/src/components/Application/MyApplications.jsx
@@ -18,7 +18,7 @@ const MyApplications = () => {
     try {
       if (user && user.role === "Employer") {
         axios
-          .get("http://localhost:5000/api/v1/application/employer/getall", {
+          .get("https://job-portal-backend-tg6f.onrender.com/api/v1/application/employer/getall", {
             withCredentials: true,
           })
           .then((res) => {
