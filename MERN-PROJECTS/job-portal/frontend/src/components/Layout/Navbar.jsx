d8a283c9649c6fcdc8fb4e3916327cb6010796b2
DURGANJALI GADAMSETTY <147343685+G-DURGANJALI@users.noreply.github.com>
2025-04-06 22:20:38 +0530

    Update Navbar.jsx

--- MERN-PROJECTS/job-portal/frontend/src/components/Layout/Navbar.jsx
+++ MERN-PROJECTS/job-portal/frontend/src/components/Layout/Navbar.jsx
@@ -13,7 +13,7 @@ const Navbar = () => {
   const handleLogout = async () => {
     try {
       const response = await axios.get(
-        "http://localhost:5000/api/v1/user/logout",
+        "https://job-portal-backend-tg6f.onrender.com/api/v1/user/logout",
         {
           withCredentials: true,
         }
