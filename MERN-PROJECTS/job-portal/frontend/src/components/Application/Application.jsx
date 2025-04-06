695c72686753f272e2318d7af7583eb483154441
DURGANJALI GADAMSETTY <147343685+G-DURGANJALI@users.noreply.github.com>
2025-04-06 22:16:45 +0530

    Update Application.jsx

--- MERN-PROJECTS/job-portal/frontend/src/components/Application/Application.jsx
+++ MERN-PROJECTS/job-portal/frontend/src/components/Application/Application.jsx
@@ -35,7 +35,7 @@ const Application = () => {
 
     try {
       const { data } = await axios.post(
-        "http://localhost:5000/api/v1/application/post",
+        "https://job-portal-backend-tg6f.onrender.comapi/v1/application/post",
         formData,
         {
           withCredentials: true,
