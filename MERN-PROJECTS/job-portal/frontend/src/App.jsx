3704dbe6057f65149e3371ece9a45ec450191d69
DURGANJALI GADAMSETTY <147343685+G-DURGANJALI@users.noreply.github.com>
2025-04-06 22:16:12 +0530

    Update App.jsx

--- MERN-PROJECTS/job-portal/frontend/src/App.jsx
+++ MERN-PROJECTS/job-portal/frontend/src/App.jsx
@@ -23,7 +23,7 @@ const App = () => {
     const fetchUser = async () => {
       try {
         const response = await axios.get(
-          "http://localhost:5000/api/v1/user/getuser",
+          "https://job-portal-backend-tg6f.onrender.com/api/v1/user/getuser",
           {
             withCredentials: true,
           }
